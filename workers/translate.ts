6e6db522cb1ff76211abd07d9b053681ac7ede69
Cody Bontecou <bontecouc@gmail.com>
2025-03-27 10:19:02 -0600

    Singleton pattern

--- workers/translate.ts
+++ workers/translate.ts
@@ -1,10 +1,33 @@
-import { pipeline } from '@xenova/transformers'
+import { pipeline, TranslationPipeline } from '@xenova/transformers'
 
-const task = 'translation'
-const model = 'Xenova/nllb-200-distilled-600M'
+class TranslationSingleton {
+    private static instance: TranslationSingleton
+    private pipelinePromise: Promise<TranslationPipeline> | null = null
+
+    private constructor() {}
+
+    public static getInstance(): TranslationSingleton {
+        if (!TranslationSingleton.instance) {
+            TranslationSingleton.instance = new TranslationSingleton()
+        }
+        return TranslationSingleton.instance
+    }
+
+    public async getTranslator(): Promise<TranslationPipeline> {
+        if (!this.pipelinePromise) {
+            this.pipelinePromise = pipeline(
+                'translation',
+                'Xenova/nllb-200-distilled-600M'
+            ) as Promise<TranslationPipeline>
+        }
+        return this.pipelinePromise
+    }
+}
 
 export async function translate(input: string) {
-    const translator = await pipeline(task, model)
+    const singleton = TranslationSingleton.getInstance()
+    const translator = await singleton.getTranslator()
+
     const translation = await translator(input, {
         tgt_lang: 'spa_Latn',
         src_lang: 'eng_Latn',
